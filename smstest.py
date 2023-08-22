b338da73878111c09000a813a89d9b99b105730f
yuxin-ng <91875860+yuxin-ng@users.noreply.github.com>
2023-08-22 21:46:59 +0800

    Update smstest.py

--- smstest.py
+++ smstest.py
@@ -2,8 +2,8 @@ import os
 from twilio.rest import Client
 
 # Replace these placeholders with your actual Twilio credentials
-account_sid = 'AC68999923900d013eaf9141d2be5a222d'
-auth_token = 'a6f299226bdbb485428dbf7335e4ec9a'
+account_sid = 'AC68999923900d013eaf9141d2be5a222d'#tochange
+auth_token = 'a6f299226bdbb485428dbf7335e4ec9a'#tochange
 from_number = '+16189120576'
 to_number = 'my_Num'
 
