23252445a3d6b7610268b651ff43ca72a0246300
yuxin-ng <ngyuxin2001@gmail.com>
2023-08-22 15:02:25 +0800

    Create smstest.py

---
+++ smstest.py
@@ -0,0 +1,20 @@
+import os
+from twilio.rest import Client
+
+# Replace these placeholders with your actual Twilio credentials
+account_sid = 'AC68999923900d013eaf9141d2be5a222d'
+auth_token = 'a6f299226bdbb485428dbf7335e4ec9a'
+from_number = '+16189120576'
+to_number = 'my_Num'
+
+# Create a Twilio client
+client = Client(account_sid, auth_token)
+
+# Send an SMS
+message = client.messages.create(
+    body='This is a test sms',
+    from_=from_number,
+    to=to_number
+)
+
+print('Message SID:', message.sid + ', Message status' + message.status)
